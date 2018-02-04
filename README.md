3dfa63b9bdcb421885325100b433f220aa387937
MarcoDellOlio <dellolio.marco@gmail.com>
2018-02-04 07:39:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# present-and-past
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(bodyParser.json());
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
